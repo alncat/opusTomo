bed224c5e1c06d3a6d16038acb134a8d297b2d3f
alncat <tluozhenwei@gmail.com>
2025-11-19 11:18:25 +0800

    Update reconstruct_subtomo.py

--- reconstruct_subtomo.py
+++ reconstruct_subtomo.py
@@ -1,24 +1,28 @@
 import os
 import subprocess
 import multiprocessing
-# 定义文件夹路径
+# define output folder path
 folder_path = './subtomos'
 def task(folder_path, filename, prefix):
     print(f"File: {filename}, Prefix: {prefix}")
     result = subprocess.run(['relion_reconstruct', '--i', os.path.join(folder_path, filename),
                              '--o', os.path.join(folder_path, prefix+'.mrc'), '--skip_gridding'],
                             capture_output=True, text=True)
+    if result.returncode != 0:
+        print("reconstruct failed!")
+        print("STDOUT:\n", result.stdout)
+        print("STDERR:\n", result.stderr)
     assert result.returncode == 0
     return result
-# 遍历文件夹中的所有文件
+# loop through all files
 with multiprocessing.Pool(processes=48) as pool:
     results = []
     for filename in os.listdir(folder_path):
-        # 检查文件是否以 _ctf.star 结尾
+        # check if the file ends with _ctf.star
         if filename.endswith('_subtomo.star'):
-        # 提取前缀
-            prefix = filename[:-len('_subtomo.star')]  # 去掉最后的 '_subtomo.star'
-            # 调用外部命令
+        # extract prefix
+            prefix = filename[:-len('_subtomo.star')]  # remove '_subtomo.star' suffix
+            # call external commands
             result = pool.apply_async(task, args=(folder_path, filename, prefix))
             results.append(result)
 
